dc25c2a82b32956b6d8ff0b9c5cce2c7f8885f27
Alan Soares <alan.ssoares@live.com>
2016-09-29 08:58:28 -0300

    Simplificando código

--- Matlab/scripts/custom_confusion.m
+++ Matlab/scripts/custom_confusion.m
@@ -7,7 +7,7 @@ files = dir('*.txt');
 for cntfiles=1:length(files)
     
     fin = fopen(files(cntfiles).name);
-    
+    gestures = ['g1','g2','g3','g4','g5','g6','g7'];
     tokens_name_file = strsplit(files(cntfiles).name,'_');
     
     if(strcmp(tokens_name_file(3), method) && ...
@@ -16,96 +16,20 @@ for cntfiles=1:length(files)
         line = fgetl(fin);
         tokens_line = strsplit(line,' ');
         if(is_vp(line))
-            if(strcmp(tokens_line(1), 'g1'))
-                mR(1,1) = mR(1,1) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g2'))
-                mR(1,2) = mR(1,2) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g3'))
-                mR(1,3) = mR(1,3) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g4'))
-                mR(1,4) = mR(1,4) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g5'))
-                mR(1,5) = mR(1,5) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g6'))
-                mR(1,6) = mR(1,6) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g7'))
-                mR(1,7) = mR(1,7) + 1;
-            end
+		c = findPosition(tokens_line(1), gestures);
+		mR(1,c) = mR(1,c) + 1;
         end
         if(is_vn(line))
-            if(strcmp(tokens_line(1), 'g1'))
-                mR(2,1) = mR(2,1) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g2'))
-                mR(2,2) = mR(2,2) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g3'))
-                mR(2,3) = mR(2,3) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g4'))
-                mR(2,4) = mR(2,4) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g5'))
-                mR(2,5) = mR(2,5) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g6'))
-                mR(2,6) = mR(2,6) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g7'))
-                mR(2,7) = mR(2,7) + 1;
-            end
+		c = findPosition(tokens_line(1), gestures);
+		mR(2,c) = mR(2,c) + 1;
         end
         if(is_fp(line))
-            if(strcmp(tokens_line(1), 'g1'))
-                mR(3,1) = mR(3,1) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g2'))
-                mR(3,2) = mR(3,2) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g3'))
-                mR(3,3) = mR(3,3) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g4'))
-                mR(3,4) = mR(3,4) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g5'))
-                mR(3,5) = mR(3,5) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g6'))
-                mR(3,6) = mR(3,6) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g7'))
-                mR(3,7) = mR(3,7) + 1;
-            end
+		c = findPosition(tokens_line(1), gestures);
+		mR(3,c) = mR(3,c) + 1;
         end
         if(is_fn(line))
-            if(strcmp(tokens_line(1), 'g1'))
-                mR(4,1) = mR(4,1) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g2'))
-                mR(4,2) = mR(4,2) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g3'))
-                mR(4,3) = mR(4,3) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g4'))
-                mR(4,4) = mR(4,4) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g5'))
-                mR(4,5) = mR(4,5) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g6'))
-                mR(4,6) = mR(4,6) + 1;
-            end
-            if(strcmp(tokens_line(1), 'g7'))
-                mR(4,7) = mR(4,7) + 1;
-            end
+		c = findPosition(tokens_line(1), gestures);
+		mR(3,c) = mR(3,c) + 1;
         end
       end
     end
@@ -113,42 +37,52 @@ for cntfiles=1:length(files)
 end
 end
 
+% Encontra o número do gesto
+function col = findPosition(g1, gestures)
+	col = 0;
+	for g2 = gestures
+		if(strcmp(g1, g2) == 1)
+			col = str2double(g1(2));
+		end
+	end
+end
+
 % Vefifica se eh um verdadeiro positivo
 function result = is_vp(line)
-    result = 0;
-    tokens = strsplit(line,' ');
-    if(strcmp(tokens(1), tokens(4)) == 1 && ... 
-        strcmp(tokens(9), '1'))
-        result = 1;
-    end
+	result = 0;
+	tokens = strsplit(line,' ');
+	if(strcmp(tokens(1), tokens(4)) == 1 && ... 
+		strcmp(tokens(9), '1'))
+		result = 1;
+	end
 end
 
 % Vefifica se eh um verdadeiro negativo
 function result = is_vn(line)
-    result = 0;
-    tokens = strsplit(line,' ');
-    if(strcmp(tokens(1), tokens(4)) == 0 && ... 
-        strcmp(tokens(9), '1'))
-        result = 1;
-    end
+	result = 0;
+	tokens = strsplit(line,' ');
+	if(strcmp(tokens(1), tokens(4)) == 0 && ... 
+		strcmp(tokens(9), '1'))
+		result = 1;
+	end
 end
 
 % Vefifica se eh um falso positivo
 function result = is_fp(line)
-    result = 0;
-    tokens = strsplit(line,' ');
-    if(strcmp(tokens(1), tokens(4)) == 0 && ... 
-        strcmp(tokens(9), '0'))
-        result = 1;
-    end
+	result = 0;
+	tokens = strsplit(line,' ');
+	if(strcmp(tokens(1), tokens(4)) == 0 && ... 
+		strcmp(tokens(9), '0'))
+		result = 1;
+	end
 end
 
 % Vefifica se eh um falso negativo
 function result = is_fn(line)
 	result = 0;
-    tokens = strsplit(line,' ');
-    if(strcmp(tokens(1), tokens(4)) == 1 && ... 
-        strcmp(tokens(9), '0'))
-        result = 1;
-    end
+	tokens = strsplit(line,' ');
+	if(strcmp(tokens(1), tokens(4)) == 1 && ... 
+		strcmp(tokens(9), '0'))
+		result = 1;
+	end
 end
