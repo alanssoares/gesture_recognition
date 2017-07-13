bb941219ff3e982f5ae452781cc43dd387373205
alanssoares <alan.soares@ufba.br>
2017-07-12 22:57:32 -0300

    Fix msrc_12 dataset
    - add origin datasets
    - fix msrc_12 dataset

--- Matlab/scripts/read_gestures.m
+++ Matlab/scripts/read_gestures.m
@@ -1,28 +1,37 @@
 function read_gestures()
 
+% Dataset cont?m na primeira coluna um id
+% M?o esquerda come?a coluna 25
+% M?o direita come?a na coluna 41
+% https://www.microsoft.com/en-us/download/details.aspx?id=52283
+
+fileID = fopen('msrc_12_raw.txt','w');
+
 df = dir('*.csv');
 total_gestures = numel(df);
 
 for i=1:total_gestures
     
     file_gesture = load(df(i).name);
-    
+    fprintf(fileID,'gesture %s hands 2\n', getNameGesture(df(i).name));
     for j=1:length(file_gesture(:,1))
-        %Left hand
-        m(j, 1) = file_gesture(j, 26);
-        m(j, 2) = file_gesture(j, 27);
-        m(j, 3) = file_gesture(j, 28);
-        %Right hand
-        m(j, 4) = file_gesture(j, 42);
-        m(j, 5) = file_gesture(j, 43);
-        m(j, 6) = file_gesture(j, 44);
+        fprintf(fileID,'%f %f %f %f %f %f\n', file_gesture(j, 26), ...
+                 file_gesture(j, 27), ...
+                 file_gesture(j, 28), ...
+                 file_gesture(j, 42), ...
+                 file_gesture(j, 43), ...
+                 file_gesture(j, 44));
     end
     
-    % save the file
-    csvwrite(sprintf('gestures_hand/%s', df(i).name), m);
+    fprintf(fileID, 'end\n');
     
-    % initialize
-    m = [];
+end
+
+% Return the name of the gesture
+function name = getNameGesture(fileName)
+    tokens = strsplit(fileName, '_');
+    token = tokens(1,3);
+    name = strcat('',token{1});%convert call to str and concat
 end
 
 end
