a41f35f9a66e1e207ad64df2e492ae22e06e4fa3
alanssoares <alan.soares@ufba.br>
2018-01-29 22:25:50 -0300

    Add result env1

---
+++ GRToolkit/execute_env1.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# ================================= AS1 =======================================
+
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_01_max.grt > result_env1/as1_arc_crv_01_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_001_max.grt > result_env1/as1_arc_crv_001_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_002_max.grt > result_env1/as1_arc_crv_002_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_003_max.grt > result_env1/as1_arc_crv_003_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_004_max.grt > result_env1/as1_arc_crv_004_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_005_max.grt > result_env1/as1_arc_crv_005_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_006_max.grt > result_env1/as1_arc_crv_006_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_007_max.grt > result_env1/as1_arc_crv_007_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_008_max.grt > result_env1/as1_arc_crv_008_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_009_max.grt > result_env1/as1_arc_crv_009_max.txt
+
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_01_max_ang.grt > result_env1/as1_arc_crv_01_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_001_max_ang.grt > result_env1/as1_arc_crv_001_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_002_max_ang.grt > result_env1/as1_arc_crv_002_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_003_max_ang.grt > result_env1/as1_arc_crv_003_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_004_max_ang.grt > result_env1/as1_arc_crv_004_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_005_max_ang.grt > result_env1/as1_arc_crv_005_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_006_max_ang.grt > result_env1/as1_arc_crv_006_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_007_max_ang.grt > result_env1/as1_arc_crv_007_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_008_max_ang.grt > result_env1/as1_arc_crv_008_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_009_max_ang.grt > result_env1/as1_arc_crv_009_max_ang.txt
+
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_01_max_crv.grt > result_env1/as1_arc_crv_01_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_001_max_crv.grt > result_env1/as1_arc_crv_001_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_002_max_crv.grt > result_env1/as1_arc_crv_002_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_003_max_crv.grt > result_env1/as1_arc_crv_003_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_004_max_crv.grt > result_env1/as1_arc_crv_004_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_005_max_crv.grt > result_env1/as1_arc_crv_005_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_006_max_crv.grt > result_env1/as1_arc_crv_006_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_007_max_crv.grt > result_env1/as1_arc_crv_007_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_008_max_crv.grt > result_env1/as1_arc_crv_008_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as1_arc_crv_009_max_crv.grt > result_env1/as1_arc_crv_009_max_crv.txt
+
+# ================================= AS2 =======================================
+
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_01_max.grt > result_env1/as2_arc_crv_01_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_001_max.grt > result_env1/as2_arc_crv_001_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_002_max.grt > result_env1/as2_arc_crv_002_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_003_max.grt > result_env1/as2_arc_crv_003_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_004_max.grt > result_env1/as2_arc_crv_004_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_005_max.grt > result_env1/as2_arc_crv_005_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_006_max.grt > result_env1/as2_arc_crv_006_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_007_max.grt > result_env1/as2_arc_crv_007_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_008_max.grt > result_env1/as2_arc_crv_008_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_009_max.grt > result_env1/as2_arc_crv_009_max.txt
+
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_01_max_ang.grt > result_env1/as2_arc_crv_01_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_001_max_ang.grt > result_env1/as2_arc_crv_001_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_002_max_ang.grt > result_env1/as2_arc_crv_002_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_003_max_ang.grt > result_env1/as2_arc_crv_003_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_004_max_ang.grt > result_env1/as2_arc_crv_004_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_005_max_ang.grt > result_env1/as2_arc_crv_005_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_006_max_ang.grt > result_env1/as2_arc_crv_006_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_007_max_ang.grt > result_env1/as2_arc_crv_007_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_008_max_ang.grt > result_env1/as2_arc_crv_008_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_009_max_ang.grt > result_env1/as2_arc_crv_009_max_ang.txt
+
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_01_max_crv.grt > result_env1/as2_arc_crv_01_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_001_max_crv.grt > result_env1/as2_arc_crv_001_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_002_max_crv.grt > result_env1/as2_arc_crv_002_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_003_max_crv.grt > result_env1/as2_arc_crv_003_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_004_max_crv.grt > result_env1/as2_arc_crv_004_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_005_max_crv.grt > result_env1/as2_arc_crv_005_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_006_max_crv.grt > result_env1/as2_arc_crv_006_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_007_max_crv.grt > result_env1/as2_arc_crv_007_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_008_max_crv.grt > result_env1/as2_arc_crv_008_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as2_arc_crv_009_max_crv.grt > result_env1/as2_arc_crv_009_max_crv.txt
+
+# ================================= AS3 =======================================
+
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_01_max.grt > result_env1/as3_arc_crv_01_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_001_max.grt > result_env1/as3_arc_crv_001_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_002_max.grt > result_env1/as3_arc_crv_002_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_003_max.grt > result_env1/as3_arc_crv_003_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_004_max.grt > result_env1/as3_arc_crv_004_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_005_max.grt > result_env1/as3_arc_crv_005_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_006_max.grt > result_env1/as3_arc_crv_006_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_007_max.grt > result_env1/as3_arc_crv_007_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_008_max.grt > result_env1/as3_arc_crv_008_max.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_009_max.grt > result_env1/as3_arc_crv_009_max.txt
+
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_01_max_ang.grt > result_env1/as3_arc_crv_01_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_001_max_ang.grt > result_env1/as3_arc_crv_001_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_002_max_ang.grt > result_env1/as3_arc_crv_002_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_003_max_ang.grt > result_env1/as3_arc_crv_003_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_004_max_ang.grt > result_env1/as3_arc_crv_004_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_005_max_ang.grt > result_env1/as3_arc_crv_005_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_006_max_ang.grt > result_env1/as3_arc_crv_006_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_007_max_ang.grt > result_env1/as3_arc_crv_007_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_008_max_ang.grt > result_env1/as3_arc_crv_008_max_ang.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_009_max_ang.grt > result_env1/as3_arc_crv_009_max_ang.txt
+
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_01_max_crv.grt > result_env1/as3_arc_crv_01_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_001_max_crv.grt > result_env1/as3_arc_crv_001_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_002_max_crv.grt > result_env1/as3_arc_crv_002_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_003_max_crv.grt > result_env1/as3_arc_crv_003_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_004_max_crv.grt > result_env1/as3_arc_crv_004_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_005_max_crv.grt > result_env1/as3_arc_crv_005_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_006_max_crv.grt > result_env1/as3_arc_crv_006_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_007_max_crv.grt > result_env1/as3_arc_crv_007_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_008_max_crv.grt > result_env1/as3_arc_crv_008_max_crv.txt
+./startDecisionTree ../Datasets/msr_action_3d/env1/features/msr3d_action_origin_as3_arc_crv_009_max_crv.grt > result_env1/as3_arc_crv_009_max_crv.txt
